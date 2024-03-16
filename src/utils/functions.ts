cf1070dec55c0492b5e13021b2f22312dd65003c
nUc1eaAr-gampRUS545 <n-mutovin2003@mail.ru>
2024-03-17 01:42:52 +0300

    corrected functions

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -3,17 +3,18 @@ type FactType = {
   length: number;
 };
 export const clear = (textInput: React.RefObject<HTMLInputElement>) => {
-    if (textInput.current) {
-      textInput.current.value = "";
-      textInput.current.focus();
-    }
-  };
- export const findFirstWord = (data:string):number =>{
-    const firstSpaceIndex = data.indexOf(' ');
-      const cursorPosition = firstSpaceIndex !== -1 ? firstSpaceIndex + 1 : data.length;
-      return cursorPosition;
+  if (textInput.current) {
+    textInput.current.value = "";
+    textInput.current.focus();
   }
-export  const setCursorAfterFirstWord = (
+};
+export const findFirstWord = (data: string): number => {
+  const firstSpaceIndex = data.indexOf(" ");
+  const cursorPosition =
+    firstSpaceIndex !== -1 ? firstSpaceIndex + 1 : data.length;
+  return cursorPosition - 1;
+};
+export const setCursorAfterFirstWord = (
   ref: React.RefObject<HTMLInputElement>,
   data: FactType
 ) => {
